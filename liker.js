898e6cc967df2dbdd10c5f74ac2afce4be165960
Fenr1sulfr <moldakhmetov00@bk.ru>
2023-10-23 13:19:46 +0600

    123

--- liker.js
+++ liker.js
@@ -1,6 +1,8 @@
-let counter = 0;
-const countElement = document.getElementById("starIcon");
-countElement.addEventListener("click",function(counter){
-    counter++
-    countElement.textContent=counter;
-})
+let counters = Array.from({ length: document.getElementsByClassName("btn btn-lg").length }, () => 0);
+const clickers = document.getElementsByClassName("btn btn-lg");
+for (let i = 0; i < clickers.length; i++) {
+    clickers[i].addEventListener("click", function() {
+        counters[i]++;
+        clickers[i].innerHTML = counters[i] + ' <i class="far fa-star" style="color: gold;"></i>';
+    });
+}   
