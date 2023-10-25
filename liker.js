d703380b1a087fe5aad70fc10e110414aec42ebd
qarangym <itsjr@vk.com>
2023-10-26 02:00:05 +0600

    liker

--- liker.js
+++ liker.js
@@ -3,6 +3,6 @@ const clickers = document.getElementsByClassName("btn btn-lg");
 for (let i = 0; i < clickers.length; i++) {
     clickers[i].addEventListener("click", function() {
         counters[i]++;
-        clickers[i].innerHTML = counters[i] + ' <i class="far fa-star" style="color: gold;"></i>';
+        clickers[i].innerHTML = '<i class="far fa-star" style="color: gold;"></i> '+counters[i] ;
     });
 }   
