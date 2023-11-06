87d96f4f778003f8916da55ab750efb89ccc6e0f
qarangym <itsjr@vk.com>
2023-11-06 15:43:05 +0600

    06 11

--- liker.js
+++ liker.js
@@ -1,8 +1,8 @@
-let counters = Array.from({ length: document.getElementsByClassName("btn btn-lg").length }, () => 0);
+let counters = Array.from({ length: document.getElementsByIdName("starIcon").length }, () => 0);
 const clickers = document.getElementsByClassName("btn btn-lg");
 for (let i = 0; i < clickers.length; i++) {
     clickers[i].addEventListener("click", function() {
         counters[i]++;
-        clickers[i].innerHTML = '<i class="far fa-star" style="color: gold;"></i> '+counters[i] ;
+        clickers[i].innerHTML = '<i class="fa fa-star" style="color: gold;"></i> '+counters[i] ;
     });
 }   
